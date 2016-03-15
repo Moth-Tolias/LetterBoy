00bc7da33f011e60380ecdd03f17ed9faa0b0a73
Su <moth.tolias@gmail.com>
2016-03-15 00:16:10 +0000

    remove out-of-date copyright info

--- backend/Text Decorruptor v1.py
+++ backend/Text Decorruptor v1.py
@@ -1,6 +1,4 @@
 import re
-
-print("Text Decorruptor v1 (c) Thomas Tootill, 2014")
 print("Be sure to run this in IDLE, so you can copypaste the text in and out.")
 TEXT = input("Put the text to be decorrupted here:")
 TEXT = re.sub('[̍̎̄̅̿̑̆̐͒͗͑̇̈̊͂̓̈́͊͋͌̃	̂̌͐̀́̋̏̒̓̔̽̉	ͣͤͥͦͧͨ	̴̵̶̡̢̧̨̛ͩͪͫͬͭͮͯ̾͛͆̀́̚̕͘͏̸̷͜͟͢͝͞͠͡҉̖̗̘̙̜̝̞̟̠̤̥̦̩̪̫̬̭̮̯̰̱̲̳̹̺̻̼͇͈͉͍͎͓͔͕͖͙͚̣ͅ]', '', TEXT)

c0ef1a8a6b09f42d4c4568d9944cbca63d81dfe9
Su <moth.tolias@gmail.com>
2016-03-15 13:09:19 +0000

    remove tabbed spaces in re.sub function
    this font is apparently not monospace, which makes it difficult to see what is really being edited...

--- backend/Text Decorruptor v1.py
+++ backend/Text Decorruptor v1.py
@@ -1,7 +1,7 @@
 import re
 print("Be sure to run this in IDLE, so you can copypaste the text in and out.")
 TEXT = input("Put the text to be decorrupted here:")
-TEXT = re.sub('[̍̎̄̅̿̑̆̐͒͗͑̇̈̊͂̓̈́͊͋͌̃	̂̌͐̀́̋̏̒̓̔̽̉	ͣͤͥͦͧͨ	̴̵̶̡̢̧̨̛ͩͪͫͬͭͮͯ̾͛͆̀́̚̕͘͏̸̷͜͟͢͝͞͠͡҉̖̗̘̙̜̝̞̟̠̤̥̦̩̪̫̬̭̮̯̰̱̲̳̹̺̻̼͇͈͉͍͎͓͔͕͖͙͚̣ͅ]', '', TEXT)
+TEXT = re.sub('[̴̵̶̡̢̧̨̛̣̍̎̄̅̿̑̆̐͒͗͑̇̈̊͂̓̈́͊͋͌̃̂̌͐̀́̋̏̒̓̔̽̉ͣͤͥͦͧͨͩͪͫͬͭͮͯ̾͛͆̀́̚̕͘͏̸̷͜͟͢͝͞͠͡҉̖̗̘̙̜̝̞̟̠̤̥̦̩̪̫̬̭̮̯̰̱̲̳̹̺̻̼͇͈͉͍͎͓͔͕͖͙͚ͅ]', '', TEXT)
 print("")
 print(TEXT)
 print("")

2c71b273474d573e344fe6335605049d05de59ad
Su <moth.tolias@gmail.com>
2016-03-15 00:04:50 +0000

    update backend with new name

--- LetterBoy_backend.py
+++ LetterBoy_backend.py
@@ -1,33 +1,33 @@
-"""Functions for the backend of the TextEdit."""
+"""Functions for the backend of LetterBoy"""
 
-def txe_standardcase():
+def lb_standardcase():
     """Capitalise the first letter of each sentence, and set all others to lowercase."""
     pass
 
-def txe_uppercase():
+def lb_uppercase():
     """Capitalise each letter."""
     pass
 
-def txe_lowercase():
+def lb_lowercase():
     """Set all letters to lowercase."""
     pass
 
-def txe_camelcase():
+def lb_camelcase():
     """Capitalise the first letter of each word, and set all others to lowercase."""
     pass
 
-def txe_staggercase():
+def lb_staggercase():
     """Alternate each character between upper- and lower-case."""
     pass
 
-def txe_jumbles_nontrobo():
+def lb_jumbles_nontrobo():
     """Jumble up text between the first and last letters in each word."""
     pass
 
-def txe_zcorrupt():
+def lb_zcorrupt():
     """Add glitch text to the plaintext."""
     pass
 
-def txe_zstrip():
+def lb_zstrip():
     """Remove glitch text."""
     pass
